0a8c65d4f66c828f4a711981838deed675affc7d
WeiWei <goalking019@gmail.com>
2020-05-29 23:52:50 +0800

    spring-boot-cloud

---
+++ spring-boot-cloud/src/test/java/com/king/springbootcloud/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.springbootcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
