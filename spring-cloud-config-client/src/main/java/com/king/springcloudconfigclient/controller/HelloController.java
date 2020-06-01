6747bc16e50ddff8ee65c6d53dbf63aecd94c09b
WeiWei <goalking019@gmail.com>
2020-06-01 13:07:16 +0800

    remove

--- spring-cloud-config-client/src/main/java/com/king/springcloudconfigclient/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/king/springcloudconfigclient/controller/HelloController.java
@@ -1,16 +1,18 @@
 package com.king.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${welcome.message}")
     private String message;
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String sayHello() {
         return message;
     }
