33cda753d7f2f98971326c46aefff438208a8789
WeiWei <goalking019@gmail.com>
2020-06-06 23:45:12 +0800

    spring boot security

---
+++ spring-boot-security/src/test/java/com/king/springbootsecurity/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
